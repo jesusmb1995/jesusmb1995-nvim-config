b0e102e6a96636f163a3bb775633a93663e2d0cb
Jesús <jesus.mb.1995@gmail.com>
2025-09-05 13:45:02 +0200

    Update savecmd

--- lua/plugins/savecmd-nvim.lua
+++ lua/plugins/savecmd-nvim.lua
@@ -1,13 +1,14 @@
 return {
   {
-    -- url = "https://github.com/jesusmb1995/savecmd",
-    dir = "/luksmap/Code/savecmd",
+    url = "https://github.com/jesusmb1995/savecmd",
+    -- dir = "/luksmap/Code/savecmd",
     lazy = true,
     cmd = { "LaunchVert", "LaunchHoriz", "LaunchFloat" },
     keys = {
-      { "<leader><A-v>", "<cmd>LaunchVert<cr>", desc = "Launch command in vertical terminal" },
+      { "<leader><A-v>", "<cmd>LaunchVert<cr>",  desc = "Launch command in vertical terminal" },
       { "<leader><A-h>", "<cmd>LaunchHoriz<cr>", desc = "Launch command in horizontal terminal" },
       { "<leader><A-i>", "<cmd>LaunchFloat<cr>", desc = "Launch command in horizontal terminal" },
+      { "<leader>rr",    "<cmd>LaunchLast<cr>",  desc = "Launch last run command" },
     },
     config = function()
       require('savecmd-nvim').setup()
