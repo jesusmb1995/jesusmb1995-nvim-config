23d4b26de286f90a5a6fabf0636a406003a2be1c
Jesús <jesus.mb.1995@gmail.com>
2026-03-12 11:10:06 +0100

    profiler

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,7 @@
 return {
   "folke/snacks.nvim",
   lazy = true,
-  cmd = { "SnacksProfilerToggle" },
+  cmd = { "SnacksProfilerToggle", "SnacksProfilerShow" },
   opts = {
     defaults = { enabled = false },
     profiler = { enabled = true },
@@ -11,5 +11,8 @@ return {
     vim.api.nvim_create_user_command("SnacksProfilerToggle", function()
       Snacks.profiler.toggle()
     end, { desc = "Toggle Snacks Profiler" })
+    vim.api.nvim_create_user_command("SnacksProfilerShow", function()
+      Snacks.profiler.scratch()
+    end, { desc = "Show Snacks Profiler results" })
   end,
 }
