7b8d68c9b760383881c278cb3f892f7f53d12a25
Jesús <jesus.mb.1995@gmail.com>
2026-03-13 10:49:38 +0100

    termux clean

--- lua/options.lua
+++ lua/options.lua
@@ -16,3 +16,14 @@ vim.opt.shortmess:append("A")
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Signal pre-warmed tmux shells that this terminal was opened from Neovim,
+-- so zsh can hide redundant cwd/git info from the prompt.
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    if vim.fn.exists("$TMUX") == 1 then
+      vim.fn.system("tmux set-environment -g NVIM_TERM_DIR " .. vim.fn.shellescape(vim.fn.getcwd()))
+    end
+  end,
+  desc = "Set NVIM_TERM_DIR in tmux env for prompt suppression",
+})
