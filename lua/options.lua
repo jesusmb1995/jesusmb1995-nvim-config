cb32d6db8d9d901a922da6868a50b61a24509e94
Jesús <jesus.mb.1995@gmail.com>
2026-06-11 10:53:40 +0200

    try undo option exec zsh

--- lua/options.lua
+++ lua/options.lua
@@ -17,19 +17,19 @@ vim.opt.shortmess:append("A")
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
--- Signal pre-warmed tmux shells that this terminal was opened from Neovim,
--- so zsh can hide redundant cwd/git info from the prompt.
-vim.api.nvim_create_autocmd("TermOpen", {
-  callback = function()
-    if vim.fn.exists("$TMUX") == 1 then
-      vim.fn.system("tmux set-environment -g NVIM_TERM_DIR " .. vim.fn.shellescape(vim.fn.getcwd()))
-    end
-    -- Unset NVIM and TMUX so oh-my-zsh tmux plugin auto-starts tmux inside :terminal
-    local buf = vim.api.nvim_get_current_buf()
-    local chan = vim.b[buf].terminal_job_id
-    if chan then
-      vim.fn.chansend(chan, "unset NVIM TMUX; exec zsh\n")
-    end
-  end,
-  desc = "Set NVIM_TERM_DIR in tmux env and enable tmux autostart in :terminal",
-})
+-- -- Signal pre-warmed tmux shells that this terminal was opened from Neovim,
+-- -- so zsh can hide redundant cwd/git info from the prompt.
+-- vim.api.nvim_create_autocmd("TermOpen", {
+--   callback = function()
+--     if vim.fn.exists("$TMUX") == 1 then
+--       vim.fn.system("tmux set-environment -g NVIM_TERM_DIR " .. vim.fn.shellescape(vim.fn.getcwd()))
+--     end
+--     -- Unset NVIM and TMUX so oh-my-zsh tmux plugin auto-starts tmux inside :terminal
+--     local buf = vim.api.nvim_get_current_buf()
+--     local chan = vim.b[buf].terminal_job_id
+--     if chan then
+--       vim.fn.chansend(chan, "unset NVIM TMUX; exec zsh\n")
+--     end
+--   end,
+--   desc = "Set NVIM_TERM_DIR in tmux env and enable tmux autostart in :terminal",
+-- })
